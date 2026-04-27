9820589170ebb3f831c907e8359a2bd6c5f2459b
justappgrabbin <bookingalexismcmillion@gmail.com>
2026-04-27 08:32:07 -0700

    Make Morph OS the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
-import MorphOS from '../src/MorphOS'
-
 export default function Page() {
-  return <MorphOS />
+  return (
+    <main style={{ margin: 0, minHeight: '100vh', background: '#0a0a0f' }}>
+      <iframe
+        src="/morph-os.html"
+        title="Morph OS"
+        style={{
+          width: '100vw',
+          height: '100vh',
+          border: 0,
+          display: 'block',
+          background: '#0a0a0f',
+        }}
+      />
+    </main>
+  )
 }
